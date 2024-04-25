053b2e33ae8b60c5b24dd6fe241a6a68f6b3af17
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-24 19:09:32 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -4,6 +4,7 @@ public class RandomLetterChooser extends RandomStringChooser
 	public RandomLetterChooser(String str)
 	{ 
 		/* to be implemented in part (b) */
+	 super(getSingleLetters(str));
 		
 	}
 	
